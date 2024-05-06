1d68649394b429e1fb3f2acf5ec597f5091f1f81
tirawat1 <tirawat.pst@gmail.com>
2024-05-06 19:46:28 +0700

    add all

--- 4.API/Program.cs
+++ 4.API/Program.cs
@@ -1,14 +1,40 @@
+using _2.Core.Extensions;
+using _3.Infra;
+using _3.Infra.Extensions;
+using _3.Infra.Database;
+
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<DataContext>(opt =>{
+  opt.UseNpgsql(builder.Configuration.GetConnectionString(
+    "DefaultConnection"));
+});
+
+#region Configure DI Container - Service Lifetimes - Infra
+
+builder.Services.AddInfraDependencyInjection(builder.Configuration);
+
+#endregion
+
+#region Configure DI Container - Service Lifetimes - Business Services
+
+builder.Services.AddCoreDependencyInjection();
+
+#endregion
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if(app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
@@ -16,29 +42,24 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapControllers();
+
+await SeedDatabase();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+async Task SeedDatabase()
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+  using (var scope = app.Services.CreateScope())
+  {
+    var dbcontext = scope.ServiceProvider.GetRequiredService<DataContext>();
+    Console.WriteLine(dbcontext.Database);
+    // Run migration scripts
+    await dbcontext.Database.MigrateAsync();
+
+    // Seed data to the project
+    await Seed.SeedData(dbcontext);
+  }
 }
